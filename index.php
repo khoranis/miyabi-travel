bc69113e0d725ea94b62003e95cdeb128358d9f0
khoranis <khoranis20@gmail.com>
2022-12-10 23:25:35 +0700

    edit new section

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <link rel="icon" type="image/x-icon" href="asset/img/m-logo.jpg">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
     <title>Miyabi Travel</title>
 </head>
 <style>
@@ -57,27 +59,30 @@
             <div class="swiper mySwiper">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide">
-                        <img src="//roam.qodeinteractive.com/wp-content/uploads/2017/08/home-4-slider-image-1.jpg" alt="">
-                        <div class="img-tick">
+                        <!-- <img src="//roam.qodeinteractive.com/wp-content/uploads/2017/08/home-4-slider-image-1.jpg" alt=""> -->
+                        <img src="asset/img/banner/felix-rostig-UmV2wr-Vbq8-unsplash.jpg" alt="">
+                        <div class="img-tick animate__animated animate__fadeInUp animate__delay-1s">
                              <img src="https://roam.qodeinteractive.com/wp-content/uploads/2017/08/home-4-slider-graphic-2.png" alt="" width="500px" height="40px">
                         </div>
                         <div class="text-wrapper">
-                            <h1 class="title">The New Frontier</h1>
-                            <button class="btn style-01">GET ROAM</button>
+                            <h1 class="title animate__animated animate__fadeInUp animate__delay-1s">The New Frontier</h1>
+                            <button class="btn style-01 animate__animated animate__fadeInUp animate__delay-1s">GET ROAM</button>
                         </div>
                     </div>
                     <div class="swiper-slide">
-                        <img src="//roam.qodeinteractive.com/wp-content/uploads/2017/08/home-4-slider-image-2.jpg" alt="">
-                        <div class="img-tick">
+                        <!-- <img src="//roam.qodeinteractive.com/wp-content/uploads/2017/08/home-4-slider-image-2.jpg" alt=""> -->
+                        <img src="asset/img/banner/fabio-comparelli-uq2E2V4LhCY-unsplash.jpg" alt="">
+                        <div class="img-tick animate__animated animate__fadeInDown animate__delay-1s">
                              <img src="https://roam.qodeinteractive.com/wp-content/uploads/2017/08/home-4-slider-graphic-2.png" alt="" width="500px" height="40px">
                         </div>
                         <div class="text-wrapper">
-                            <h1 class="title">Spirit of Exploration</h1>
-                            <button class="btn style-01">GET ROAM</button>
+                            <h1 class="title animate__animated animate__fadeInDown animate__delay-1s">Spirit of Exploration</h1>
+                            <button class="btn style-01 animate__animated animate__fadeInDown animate__delay-1s">GET ROAM</button>
                         </div>
                     </div>
                     <div class="swiper-slide">
-                        <img src="//roam.qodeinteractive.com/wp-content/uploads/2017/08/home-4-slider-image-3.jpg" alt="">
+                        <!-- <img src="//roam.qodeinteractive.com/wp-content/uploads/2017/08/home-4-slider-image-3.jpg" alt=""> -->
+                        <img src="asset/img/banner/redd-f-rjfOdiB7k-E-unsplash.jpg" alt="">
                         <div class="img-tick">
                              <img src="https://roam.qodeinteractive.com/wp-content/uploads/2017/08/home-4-slider-graphic-2.png" alt="" width="500px" height="40px">
                         </div>
@@ -122,18 +127,179 @@
         </div>
    <!-- END CONTENT -->
 
+   <!-- START IMAGE GRID -->
+        <div class="section-02">
+            <div class="section-02-column" style="background-color:#aaa;">
+                <div class="photo">
+                    <img src="asset/img/lisheng-chang-m9BBVrPl87M-unsplash.jpg" alt="">
+                </div>
+            </div>
+            <div class="section-02-column" style="background-color:#8fd5ef;">
+                <div class="section-02-column-small">
+                    <div class="photo">
+                        <img src="asset/img/anantachai-saothong-0DKDTSFBXc8-unsplash.jpg" alt="">
+                    </div>
+                </div>
+                <div class="section-02-column-small">
+                    <div class="photo">
+                        <img src="asset/img/jesse-schoff-74yB8FhZyWc-unsplash.jpg" alt="">
+                    </div>
+                </div>
+                <div class="section-02-column-small">
+                    <div class="photo">
+                        <img src="asset/img/polina-kocheva-OeELxA2wpc8-unsplash.jpg" alt="">
+                    </div>
+                </div>
+                <div class="section-02-column-small">
+                    <div class="photo">
+                        <img src="asset/img/robin-noguier-sydwCr54rf0-unsplash.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+   <!-- END IMAGE GRID -->
+
+   <!-- START ICON -->
+        <div class="section-03">
+            <div class="section-03-column">
+                <div class="section-card">
+                    <div class="section-icon">
+                        <a href="#" class=""><i class="fa-regular fa-gem"></i></i></a>
+                    </div>
+                    <a href="#" class="h-card">Color it all</a>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, ipsa! Dolores obcaecati ad aperiam dolorem placeat hic consectetur.</p>
+                </div>
+            </div>
+            <div class="section-03-column">
+                <div class="section-card">
+                    <div class="section-icon">
+                        <a href="#" class=""><i class="fa-solid fa-pencil"></i></a>
+                    </div>
+                    <a href="#" class="h-card">Branding projects</a>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, ipsa! Dolores obcaecati ad aperiam dolorem placeat hic consectetur.</p>
+                </div>
+            </div>
+            <div class="section-03-column">
+                <div class="section-card">
+                    <div class="section-icon">
+                        <a href="#" class=""><i class="fa-regular fa-star"></i></a>
+                    </div>
+                    <a href="#" class="h-card">Promotion & event</a>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, ipsa! Dolores obcaecati ad aperiam dolorem placeat hic consectetur.</p>
+                </div>
+            </div>
+            <div class="section-03-column">
+                <div class="section-card">
+                    <div class="section-icon">
+                        <a href="#" class=""><i class="fa-regular fa-bookmark"></i></a>
+                    </div>
+                    <a href="#" class="h-card">Advertising</a>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, ipsa! Dolores obcaecati ad aperiam dolorem placeat hic consectetur.</p>
+                </div>
+            </div>
+        </div>
+   <!-- END ICON -->
+
+   <!-- START IMAGE BOX -->
+        <div class="section-04">
+            <div class="section-04-column">
+                <img src="asset/img/hank-paul-xF-3yQQaNfU-unsplash.jpg" alt="" class="section-04-image">
+                <div class="section-04-des">
+                    <div class="section-04-text">
+                        <a href="#">Khoranis</a>
+                        <p>Front-End</p>
+                        <div class="section-04-icon">
+                            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-github"></i></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-04-column">
+                <img src="asset/img/mukuko-studio-tPKQwYHy8q4-unsplash.jpg" alt="" class="section-04-image">
+                <div class="section-04-des">
+                    <div class="section-04-text">
+                        <a href="#">Wasin</a>
+                        <p>Back-End</p>
+                        <div class="section-04-icon">
+                            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-github"></i></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-04-column">
+                <img src="asset/img/luke-porter-rg1Z9NtEa80-unsplash.jpg" alt="" class="section-04-image">
+                <div class="section-04-des">
+                    <div class="section-04-text">
+                        <a href="#">Khoranis</a>
+                        <p>Developer</p>
+                        <div class="section-04-icon">
+                            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-github"></i></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-04-column">
+                <img src="asset/img/joshua-earle-8MbdD0pHXGY-unsplash.jpg" alt="" class="section-04-image">
+                <div class="section-04-des">
+                    <div class="section-04-text">
+                        <a href="#">Wasin</a>
+                        <p>Developer</p>
+                        <div class="section-04-icon">
+                            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a href="#"><i class="fa-brands fa-github"></i></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+   <!-- END IMAGE BOX -->
+
+   <!-- START NUMBER -->
+        <div class="section-05">
+            <div class="section-05-column">
+                <div class="section-05-des">
+                    <h1>1982</h1>
+                    <p>Cups of coffee & tea</p>
+                </div>
+            </div>
+            <div class="section-05-column">
+                <div class="section-05-des">
+                    <h1>28+</h1>
+                    <p>Years of experience</p>
+                </div>
+            </div>
+            <div class="section-05-column">
+                <div class="section-05-des">
+                    <h1>219k</h1>
+                    <p>Hours on projects </p>
+                </div>
+            </div>
+            <div class="section-05-column">
+                <div class="section-05-des">
+                    <h1>121</h1>
+                    <p>National awards</p>
+                </div>
+            </div>
+        </div>
+   <!-- END NUMBER -->
+
+   <!-- <div class="mt-5">
+    <h1>pppppppppppppppppppppppppp</h1>
+   </div> -->
+
 
   
-    
-
-
-
-
-
-
-   
-
-
 
 
     <script src="asset/js/bootstrap.bundle.min.js"></script>
